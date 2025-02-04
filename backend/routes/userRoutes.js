af9a21b192b1720d0a937cae93cf37e317d75b06
ddavid5UW <ddavid5@wisc.edu>
2025-02-03 22:34:27 -0600

    fixed userRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import signupOrLogin from '../controllers/userController.js';
+import signupOrLogin from '../api/user.js';
 const router = express.Router();
 
-router.post('/', signupOrLogin);  // Use the same '/signup' route for both actions
+router.post('/', signupOrLogin);  // Handles POST request for signup or login
 
 export default router;
